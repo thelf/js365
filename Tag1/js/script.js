247db89ca74dfac3e3a80c3fda218aa426e82520
Thomas Helfrich <info@helfrich-media.de>
2017-07-05 20:35:48 +0200

    Tag 1

---
+++ Tag1/js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let userVorname = prompt( "Bitte geben Sie Ihren Vornamen ein" );
+let userName = prompt( "Bitte geben Sie Ihren Nachnamen ein" );
+
+console.log( "Hallo " + userVorname + ". " + "Dein Nachname lautet " + userName + "." );
+console.log( `Hallo ${userVorname}. Dein Nachname lautet ${userName}.` );
+console.log( `Hallo + userVorname. + Dein Nachname lautet ${userName}` );
