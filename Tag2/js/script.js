09ce909c2a1e155d821653bde4ec26928d1862f1
Thomas Helfrich <info@helfrich-media.de>
2017-07-06 12:08:55 +0200

    Übung Tag 2

---
+++ Tag2/js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let strecke = Number(prompt( "Bitte geben Sie die gefahrenen Kilometer ein." ));
+let liter = Number(prompt( "Bitte geben Sie nun die verbrauchte Benzinmenge ein." ));
+let tankGroesse = Number(prompt( "Bitte geben Sie abschließend auch noch Ihre Tankgröße an." ));
+
+const KM_ANGABE = 100
+
+let verbrauch = (liter / strecke) * KM_ANGABE;
+let reichweite = tankGroesse * (strecke / verbrauch);
+
+alert( `Der Kraftstoffverbrauch auf 100 km beträgt ${verbrauch} Liter. 
+Ihre maximale Reichweite bei einer Tankgröße von ${tankGroesse} Litern beträgt ${reichweite} Kilometer:` );
