caf67658fdff863865b2cc9a00982ab6aa0f242f
Thomas Helfrich <info@helfrich-media.de>
2017-07-14 13:44:28 +0200

    Syntax Fehler
    Syntax Fehler bei der Verkettung von String und Variable korrigiert

--- Tag1/js/script.js
+++ Tag1/js/script.js
@@ -5,4 +5,4 @@ let userName = prompt( "Bitte geben Sie Ihren Nachnamen ein" );
 
 console.log( "Hallo " + userVorname + ". " + "Dein Nachname lautet " + userName + "." );
 console.log( `Hallo ${userVorname}. Dein Nachname lautet ${userName}.` );
-console.log( `Hallo + userVorname. + Dein Nachname lautet ${userName}` );
+console.log( "Hallo " + userVorname + ` Dein Nachname lautet ${userName}` );
