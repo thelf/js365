8b67b2a1554d19554913d09a37110f3eaedb77e9
Thomas Helfrich <info@helfrich-media.de>
2017-12-01 14:47:41 +0100

    Add files via upload

---
+++ Woche3/Tag15/js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let vorname = prompt("Bitte gib deinen Vornamen ein.");
+let nachname = prompt("Nun gib bitte deinen Nachnamen ein.");
+
+let ergebnis = (vorname.length * nachname.length);
+
+alert(`Die Länge deines Vornamens multipliziert mit der Länge deines Nachnamens ergibt die Zahl ${ergebnis}.`);
+
---
+++ Woche3/Tag17/js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const MWST = 1.20;
+
+let nettoPreis = Number(prompt ("Bitte geben Sie den Netto Preis ein."));
+let bruttoPreis = nettoPreis * MWST;
+
+alert(`Der Bruttopreis von ${nettoPreis},- € beträgt ${bruttoPreis},- €`);
