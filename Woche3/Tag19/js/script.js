a5a70ff26126c91b7ab0f43f4877d9e97b7296ad
Thomas Helfrich <info@helfrich-media.de>
2017-12-06 20:41:12 +0100

    if-else-Verzweigung

---
+++ Woche3/Tag19/js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+let userName = prompt("Hallo lieber Teilnehmer, verrate uns bitte deinen Namen.");
+
+alert(`Willkommen ${userName}. Wir freuen uns, dass du teilnimmst.`);
+
+let userAnswer = prompt(`Nun geht es los ${userName}. Fühlst du dich fit genug um zu starten? \n\nDann gib bitte "Ja" ein. Oder breche mit "Nein" das Quiz ab.`);
+
+if(userAnswer === "Ja"){
+
+  let firstQuestion = prompt(`Klasse, das Quiz beginnt nun! Hier ist die erste Frage ${userName}. Welcher Operator hat die höhere Priorität: "*" oder "?"`);
+
+    if(firstQuestion === "*"){
+      alert(`Die Antwort ist richtig ${userName}!`);
+
+      let secondQuestion = prompt(`Hier kommt die zweite Frage ${userName}. \n\nUm welchen Datentyp handelt es sich bei "Haus" ? Um den Datentyp "string" oder "number"?`);
+
+        if(secondQuestion === "string"){
+          alert(`Die Antwort ist richtig ${userName}!`);
+        } else {
+          alert(`Die Antwort ist leider falsch ${userName}!`);
+        }
+
+    } else {
+      alert(`Die Antwort ist leider falsch ${userName}!`);
+    }
+
+} else {
+  alert(`Schade, wir wünschen dir noch einen schönen Tag ${userName}.`);
+}
