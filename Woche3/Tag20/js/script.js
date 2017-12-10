73cf804a52ff6a8effeece0a8e28d61a998bb9bd
Thomas Helfrich <info@helfrich-media.de>
2017-12-10 09:26:41 +0100

    Quellcode formatiert (JSCS Linter)

--- Woche3/Tag20/js/script.js
+++ Woche3/Tag20/js/script.js
@@ -2,22 +2,32 @@
 
 const PROZENT = 100;
 
-let userWeightYesterday = Number(prompt(`Hallo lieber Benutzer, bitte verrate uns dein gestriges Gewicht in Kilogramm.`));
+let userWeightYesterday = Number(prompt(`Hallo lieber Benutzer, bitte verrate
+uns dein gestriges Gewicht in Kilogramm.`));
+
 let userWeightToday = Number(prompt(`Und nun verrate uns bitte dein Gewicht von heute.`));
 
-if(userWeightToday < userWeightYesterday){
+if (userWeightToday < userWeightYesterday) {
 
-  let userWeightLess =((userWeightToday - userWeightYesterday ) / userWeightYesterday  * PROZENT).toFixed(2);
+  let userWeightLess = ((userWeightToday - userWeightYesterday)
+  / userWeightYesterday
+  * PROZENT)
+  .toFixed(2);
 
-  alert(`Prima du hast um ${userWeightLess}% abgenommen. Das entspricht ${userWeightYesterday - userWeightToday}kg.`);
+  alert(`Prima du hast um ${userWeightLess}% abgenommen.
+  Das entspricht ${userWeightYesterday - userWeightToday}kg.`);
 }
 
-if(userWeightToday > userWeightYesterday){
-    let userWeightMore =((userWeightToday - userWeightYesterday ) / userWeightYesterday  * PROZENT).toFixed(2);
+if (userWeightToday > userWeightYesterday) {
+  let userWeightMore = ((userWeightToday - userWeightYesterday)
+  / userWeightYesterday
+  * PROZENT)
+  .toFixed(2);
 
-    alert(`Schade, du hast um ${userWeightMore}% zugenommen. Das entspricht ${userWeightToday - userWeightYesterday}kg.`);
+  alert(`Schade, du hast um ${userWeightMore}% zugenommen.
+  Das entspricht ${userWeightToday - userWeightYesterday}kg.`);
 }
 
-if(userWeightToday === userWeightYesterday){
-  alert(`Ok, dein Gewicht hat sich nicht verändert.`)
+if (userWeightToday === userWeightYesterday) {
+  alert(`Ok, dein Gewicht hat sich nicht verändert.`);
 }
