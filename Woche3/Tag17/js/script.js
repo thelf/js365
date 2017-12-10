c97d468c6197b6b2144036119c5d018315e7e9bc
Thomas Helfrich <info@helfrich-media.de>
2017-12-10 16:33:55 +0100

    Quellcode formatiert

--- Woche3/Tag17/js/script.js
+++ Woche3/Tag17/js/script.js
@@ -2,7 +2,7 @@
 
 const MWST = 1.20;
 
-let nettoPreis = Number(prompt ("Bitte geben Sie den Netto Preis ein."));
+let nettoPreis = Number(prompt(`Bitte geben Sie den Netto Preis ein.`));
 let bruttoPreis = nettoPreis * MWST;
 
 alert(`Der Bruttopreis von ${nettoPreis},- € beträgt ${bruttoPreis},- €`);
