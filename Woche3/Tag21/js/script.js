d174c5b43d3e0ef7006cf658fb04f04ffceef8f5
Thomas Helfrich <info@helfrich-media.de>
2017-12-14 08:26:22 +0100

    Validierung der Benutzereingabe ob es sich um eine Zahl handelt.

--- Woche3/Tag21/js/script.js
+++ Woche3/Tag21/js/script.js
@@ -2,16 +2,25 @@
 
 let year = Number(prompt(`Hallo lieber Benutzer, gib bitte ein Jahr ein.`));
 
-if (year % 400 === 0) {
-  alert(`Das Jahr ${year} ist ein Schaltjahr!`);
+/*Validierung
+  Abfrage: Ist der Inhalt der Variable "year" keine Zahl? Wenn der Inhalt keine
+  Zahl ist, ist die Bedingung erfüllt und der Rückgabewert ist true. Die alert
+  Meldung wird ausgegeben.
+*/
+if (isNaN(year)) {
+  alert(`Bitte geben Sie eine Zahl ein!`);
 }else {
-  if (year % 100 === 0) {
-    alert(`Das Jahr ${year} ist kein Schaltjahr!`);
+  if (year % 400 === 0) {
+    alert(`Das Jahr ${year} ist ein Schaltjahr!`);
   }else {
-    if (year % 4 === 0) {
-      alert(`Das Jahr ${year} ist ein Schaltjahr!`);
-    }else {
+    if (year % 100 === 0) {
       alert(`Das Jahr ${year} ist kein Schaltjahr!`);
+    }else {
+      if (year % 4 === 0) {
+        alert(`Das Jahr ${year} ist ein Schaltjahr!`);
+      }else {
+        alert(`Das Jahr ${year} ist kein Schaltjahr!`);
+      }
     }
   }
 }
