6b54a3bed5e98860efb621e98a949fe36a59a1aa
Thomas Helfrich <info@helfrich-media.de>
2017-12-17 19:36:39 +0100

    if-else-Verzweigung mit logischen Operatoren - Schaltjahr berechnen

---
+++ Woche4/Tag25/js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let year = Number(prompt(`Hallo lieber Benutzer, gib bitte ein Jahr ein.`));
+
+/*Validierung
+  Abfrage: Ist der Inhalt der Variable "year" keine Zahl? Wenn der Inhalt keine
+  Zahl ist, ist die Bedingung erfüllt und der Rückgabewert ist true. Die alert
+  Meldung wird ausgegeben.
+*/
+if (isNaN(year)) {
+  alert(`Bitte geben Sie eine Zahl ein!`);
+}else {
+  if (year % 400 === 0 || year % 4 === 0 && year % 100 !== 0) {
+    alert(`Das Jahr ${year} ist ein Schaltjahr!`);
+  }else {
+    alert(`Das Jahr ${year} ist kein Schaltjahr!`);
+  }
+}
