5a826d1a03465a9d23501c2ac200a501daaaaa43
Thomas Helfrich <info@helfrich-media.de>
2017-12-27 12:01:45 +0100

    Position eines Strings anzeigen mit indexOf

---
+++ Monat 2/Woche 5/Tag31/js/script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+let text = "Heuhaufen Heuhaufen Heuhaufen Heuhaufen Heuhaufen Heuhaufen Heuhaufen Nadel Heuhaufen Heuhaufen Heuhaufen Heuhaufen Heuhaufen Heuhaufen Heuhaufen Heuhaufen Heuhaufen";
+
+console.log(text.indexOf("Nadel"));
