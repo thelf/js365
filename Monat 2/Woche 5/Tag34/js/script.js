a165669483161b03192bc439bfd651136aee9d02
Thomas Helfrich <info@helfrich-media.de>
2017-12-29 09:45:14 +0100

    Arbeiten mit Funktionen - Newsletter personalisieren.

---
+++ Monat 2/Woche 5/Tag34/js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let showNewsletterFor = (userName) => {
+  console.log(`Hallo ${userName},`);
+  console.log(`Wir möchten Sie mit diesem Newsletter gerne über unsere neuen Angebote informierem. Besuchen Sie doch unsere neue Webseite. `);
+  console.log("Mit freundlichen Grüßen,\nIhr freundlicher Nachbar");
+};
+
+showNewsletterFor("Anton");
+showNewsletterFor("Herbert");
+showNewsletterFor("Paul");
